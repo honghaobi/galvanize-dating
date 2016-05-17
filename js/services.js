aa7cf66f8cdb6a222f3153be3819c9dd387af212
henrybi <henrybi@u.washington.edu>
2016-05-17 16:47:22 -0700

    log in works

---
+++ js/services.js
@@ -0,0 +1,35 @@
+app.service("UserService", function($http, $location, $q, $window){
+  var baseUrl = 'http://galvanize-student-apis.herokuapp.com/gdating/';
+  return {
+    signup: function(user){
+      return $http.post(baseUrl + 'auth/register', user);
+    },
+    login: function(user){
+      return $http.post(baseUrl + 'auth/login', user);
+    },
+    setCurrentUser: function(data){
+      $window.localStorage.setItem("token",data.data.token);
+      $window.localStorage.setItem("user",JSON.stringify(data.data.user));
+    },
+    getCurrentUser: function(){
+      var dfd = $q.defer();
+      dfd.resolve(JSON.parse($window.localStorage.getItem("user")));
+      return dfd.promise;
+    },
+    logout: function(){
+      localStorage.clear();
+    }
+    // getAllUsers: function(){
+    //   return $http.get("/api/users/");
+    // },
+    // getSingleUser: function(id){
+    //   return $http.get("/api/users/" + id);
+    // },
+    // editUser: function(user){
+    //   return $http.put("/api/users/" + user.data.id, user.data);
+    // },
+    // removeUser: function(id){
+    //   return $http.delete("/api/users/" + id);
+    // }
+  };
+});
